ab9f9e792988e79a243618c3e52d47473953ae63
lixiang <1290708840@qq.com>
2019-11-13 15:23:56 +0800

    444

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,9 @@
-package cn.csg.upt.portal.controller;
-
+package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
- 
     @GetMapping(value = "/hello")
     public String say() {
         return "hello world";
